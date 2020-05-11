ca312986cd26a10ad366b59d4181156688e518fc
dirocchini <roquinni@gmail.com>
2020-05-11 19:28:18 -0300

    refactoring model course

--- test/Domain.Test/Courses/CourseTest.cs
+++ test/Domain.Test/Courses/CourseTest.cs
@@ -16,24 +16,30 @@ namespace Domain.Test.Courses
             {
                 Name = "course 1",
                 Workload = (double)80,
-                Audience = "students",
+                Audience = Audience.Student,
                 Value = (double)20
             };
-
 
             var course = new Course(expectedCourse.Name, expectedCourse.Workload, expectedCourse.Audience, expectedCourse.Value);
             expectedCourse.ToExpectedObject().ShouldMatch(course);
         }
     }
 
+    public enum Audience
+    {
+        Student,
+        Developer,
+        Employee,
+        Director
+    }
     public class Course
     {
         public string Name { get; private set; }
         public double Workload { get; private set; }
-        public string Audience { get; private set; }
+        public Audience Audience { get; private set; }
         public double Value { get; private set; }
 
-        public Course(string name, in double workload, string audience, in double value)
+        public Course(string name, in double workload, Audience audience, in double value)
         {
             Name = name;
             Workload = workload;
